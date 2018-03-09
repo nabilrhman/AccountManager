075526c0ee36ce7f1e476e2accc3e85e697da1b2
Nabil Rahman <nabilr@outlook.com>
2018-03-09 14:51:50 -0700

    Adds assword, email and date validator to InputValidator.

--- src/validate/InputValidator.java
+++ src/validate/InputValidator.java
@@ -10,21 +10,119 @@ public static class InputValidator
 {
 
 
+    private static final String USERNAME_PATTERN = "^[a-z0-9_-]{3,15}$";
+    private static final String PASSWORD_PATTERN = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})";
+    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+            + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    private static final String DATE_PATTERN = "(0?[1-9]|[12][0-9]|3[01])/(0?[1-9]|1[012])/((19|20)\\d\\d)";
+
     private Pattern pattern;
     private Matcher matcher;
-    private static final String USERNAME_PATTERN = "^[a-z0-9_-]{3,15}$";
+
     /**
      * Validate username with regular expression
+     *
      * @param username username for validation
      * @return true valid username, false invalid username
      */
-    public boolean validateUsername(final String username)
+    public boolean validateUsername(String username)
     {
 
         pattern = Pattern.compile(USERNAME_PATTERN);
-
         matcher = pattern.matcher(username);
         return matcher.matches();
+    }
+
+    /**
+     * Validate password with regular expression
+     *
+     * @param password password for validation
+     * @return true valid password, false invalid password
+     */
+    public boolean validatePassword(String password)
+    {
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+        matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+
+    /**
+     * Validate email with regular expression
+     *
+     * @param email email for validation
+     * @return true valid hex, false invalid hex
+     */
+    public boolean validateEmail(String email)
+    {
+        pattern = Pattern.compile(EMAIL_PATTERN);
+        matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public boolean validateBirthdate(String date)
+    {
+
+        matcher = pattern.matcher(date);
+
+        if (matcher.matches())
+        {
+
+            matcher.reset();
+
+            if (matcher.find())
+            {
+
+                String day = matcher.group(1);
+                String month = matcher.group(2);
+                int year = Integer.parseInt(matcher.group(3));
+
+                if (day.equals("31") &&
+                        (month.equals("4") || month.equals("6") || month.equals("9") ||
+                                month.equals("11") || month.equals("04") || month.equals("06") ||
+                                month.equals("09")))
+                {
+                    return false; // only 1,3,5,7,8,10,12 has 31 days
+                }
+                else if (month.equals("2") || month.equals("02"))
+                {
+                    //leap year
+                    if (year % 4 == 0)
+                    {
+                        if (day.equals("30") || day.equals("31"))
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            return true;
+                        }
+                    }
+                    else
+                    {
+                        if (day.equals("29") || day.equals("30") || day.equals("31"))
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            return true;
+                        }
+                    }
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
     }
 
 }
